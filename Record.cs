faf649e2cac76153a6475883b2c371deafe1b930
Oleh <olehbobeliak@gmail.com>
2021-08-17 18:09:12 +0300

    Context and record classes created.

---
+++ Record.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Snake
+{
+    class Record
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+    }
+}
